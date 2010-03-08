6ed6bfc7381c3b318e339dc6b5258e6593322fe2
Chris Laprun <metacosm@gmail.com>
2010-03-08 16:05:36 +0000

    - Moved common methods to ContainerURL. Polymorphism is good!

--- api/src/main/java/org/gatein/pc/api/PortletURL.java
+++ api/src/main/java/org/gatein/pc/api/PortletURL.java
@@ -1,28 +1,26 @@
-/******************************************************************************
- * JBoss, a division of Red Hat                                               *
- * Copyright 2006, Red Hat Middleware, LLC, and individual                    *
- * contributors as indicated by the @authors tag. See the                     *
- * copyright.txt in the distribution for a full listing of                    *
- * individual contributors.                                                   *
- *                                                                            *
- * This is free software; you can redistribute it and/or modify it            *
- * under the terms of the GNU Lesser General Public License as                *
- * published by the Free Software Foundation; either version 2.1 of           *
- * the License, or (at your option) any later version.                        *
- *                                                                            *
- * This software is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
- * Lesser General Public License for more details.                            *
- *                                                                            *
- * You should have received a copy of the GNU Lesser General Public           *
- * License along with this software; if not, write to the Free                *
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
- ******************************************************************************/
+/*
+ * JBoss, a division of Red Hat
+ * Copyright 2010, Red Hat Middleware, LLC, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.gatein.pc.api;
-
-import org.gatein.pc.api.ContainerURL;
 
 /**
  * @author <a href="mailto:julien@jboss.org">Julien Viet</a>
@@ -30,17 +28,4 @@ import org.gatein.pc.api.ContainerURL;
  */
 public interface PortletURL extends ContainerURL
 {
-   /**
-    * Returns the mode that may be null.
-    *
-    * @return the mode
-    */
-   org.gatein.pc.api.Mode getMode();
-
-   /**
-    * Returns the window state that may be null.
-    *
-    * @return the window state
-    */
-   org.gatein.pc.api.WindowState getWindowState();
 }
