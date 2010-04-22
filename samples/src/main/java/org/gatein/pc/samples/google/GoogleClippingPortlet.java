0cc644ac769c2f4c808972943b4a697844efa228
Chris Laprun <metacosm@gmail.com>
2010-04-22 22:23:54 +0000

    - GTNPC-20: Fixed incorrect markup in doEdit.

--- samples/src/main/java/org/gatein/pc/samples/google/GoogleClippingPortlet.java
+++ samples/src/main/java/org/gatein/pc/samples/google/GoogleClippingPortlet.java
@@ -1,25 +1,25 @@
-/******************************************************************************
- * JBoss, a division of Red Hat                                               *
- * Copyright 2008, Red Hat Middleware, LLC, and individual                    *
- * contributors as indicated by the @authors tag. See the                     *
- * copyright.txt in the distribution for a full listing of                    *
- * individual contributors.                                                   *
- *                                                                            *
- * This is free software; you can redistribute it and/or modify it            *
- * under the terms of the GNU Lesser General Public License as                *
- * published by the Free Software Foundation; either version 2.1 of           *
- * the License, or (at your option) any later version.                        *
- *                                                                            *
- * This software is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
- * Lesser General Public License for more details.                            *
- *                                                                            *
- * You should have received a copy of the GNU Lesser General Public           *
- * License along with this software; if not, write to the Free                *
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
- ******************************************************************************/
+/*
+ * JBoss, a division of Red Hat
+ * Copyright 2010, Red Hat Middleware, LLC, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 
 package org.gatein.pc.samples.google;
 
@@ -137,10 +137,10 @@ public class GoogleClippingPortlet extends GenericPortlet
       renderResponse.getWriter().print(
          "<div align='center'>\n" +
             "   <br/>\n" +
-            "   <form method='post' action='" + renderResponse.createActionURL() + "'\n" +
+            "   <form method='post' action='" + renderResponse.createActionURL() + "'>\n" +
             "      <font class='portlet-font'>Change location (zip code): </font>\n" +
             "      <input class='portlet-form-input-field' type='text' value='' size='12' name='" + ZIP + "'/>\n" +
-            "      <input class='portlet-form-input-field' type='submit' name='submit' value='submit'>\n" +
+            "      <input class='portlet-form-input-field' type='submit' name='submit' value='submit'/>\n" +
             "   </form>\n" +
             "</div>");
    }
